2d2b686de9dc7988d39f1d57f560d12e8034307d
Rina23639 <153788278+Rina23639@users.noreply.github.com>
2023-12-14 09:55:40 +0700

    upload  praktikum-a1

---
+++ praktikum-1.py
@@ -0,0 +1,45 @@
+print('praktikum-1')
+print('Nama Lengkap : Indra Wahyulla')
+print('NIM          : 231031017')
+print('Prodi        : Sistem Informasi')
+
+print()
+
+#print('Angkatan'+str(x))-mengubah int menjadi string
+x=2023
+print('Angkatan',x) 
+
+#untuk mengetahui tipe data dari sebuah variable
+print()
+
+a=10
+print('Data Adalah : ',a)
+print('Tipe Data a adalah',type(a))
+print()
+
+b=10.0
+print('Data Adalah : ',b)
+print('Tipe Data b adalah',type(b))
+print()
+
+kampus = 'Institut Teknologi Bj Habibie'
+print('Data Adalah : ',kampus)
+print('Tipe Data Kampus adalah',type(kampus))
+print()
+
+c=complex(2,3)
+print('Data Adalah : ',c)
+print('Tipe Data Kampus adalah',type(c))
+print()
+
+log=True
+print('Data Adalah : ',log)
+print('Tipe Data Kampus adalah',type(log))
+print()
+
+#menjalankan operasi perhitungan
+p=20
+q=23
+hasil=p+q
+print('Hasil',p,'+',q,'=',hasil)
+print()
