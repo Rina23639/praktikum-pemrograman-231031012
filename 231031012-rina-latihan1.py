1e40a65709d354c8ce020980b300bd5cd3449d0b
Rina23639 <153788278+Rina23639@users.noreply.github.com>
2023-12-21 09:03:48 +0700

    latihan-a1.py

---
+++ 231031012-rina-latihan1.py
@@ -0,0 +1,18 @@
+print('231031021-aulia-latihan1\n')
+
+nilai = 70
+batas_lulus = 65
+
+if nilai > batas_lulus:
+    print('Selamat, kamu lulus!')
+
+nilai = 50
+batas_lulus = 65
+print()
+
+if nilai > batas_lulus:
+    print('Selamat, kamu lulus!')
+
+
+else:
+    print('Sayang sekali, kamu tidak lulus!')
