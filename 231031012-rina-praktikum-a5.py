01d2112414e580b172306341727315d9de86dfb1
Rina23639 <153788278+Rina23639@users.noreply.github.com>
2023-12-21 09:12:22 +0700

    praktikum-a5.py

---
+++ 231031012-rina-praktikum-a5.py
@@ -0,0 +1,37 @@
+print('231031021-aulia-praktikum-a5\n')
+
+a = True
+
+counter = 0
+
+while a:
+    print('Menjalankan Programan')
+
+    counter += 1
+
+    if counter == 5:
+        break
+
+ #========PENJELASAN========#
+
+#Programan menggunakan loop 'while' yang akan berjalan selama nilai 'a' adalah true.
+#Setiap iterasi mencetak "Menjalankan Programan".
+#Ada penambahan pada variebel 'counter' setiap kali loop dijalankan.
+#Jika 'counter' mencapai 5, loop dihentikan dengan pernyataan 'break'.
+#Hasil running kode ini akan mencetak "Menjalankan Programan" sebanyak lima kali.
+
+#===========================#
+print()
+#========MEMCOBA 10 KALI========#
+
+a = True
+
+counter = 0
+
+while a:
+    print('Menjalankan Programan')
+
+    counter += 1
+
+    if counter == 10:
+        break
