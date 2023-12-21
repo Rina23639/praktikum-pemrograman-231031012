5e416a2589abe3d6b71e7b68fe0998e8e61c3fe9
Rina23639 <153788278+Rina23639@users.noreply.github.com>
2023-12-21 09:07:26 +0700

    praktikum-a1.py

---
+++ praktikum-a1.py
@@ -0,0 +1,80 @@
+# Print Nama Lengkap, NIM, Prodi
+print('Nama Lengkap: Andi Aulia Shafira')
+print('NIM         : 231031021')
+print('Prodi       : Sistem Informasi')
+print()
+x=2023
+print('Angkatan',x)
+print()
+a=10
+print('Data adalah: ',a)
+print('tipe data adalah',type(a))
+print ()
+
+b=10.0
+print('Data adalah: ',b)
+print('tipe data adalah',type (b))
+print()
+
+kampus = 'Institut Teknologi BJ Habibie'
+print('Data adalah: ',kampus)
+print('tipe data kampus adalah',type(kampus))
+print()
+
+c=complex(2,3)
+print('Data adalah: ',c)
+print('tipe data adalah',type (c))
+print()
+
+log = True
+print('Data adalah: ',log)
+print('tipe data adalah',type(log))
+print()
+
+p=20
+q=23
+hasil = p+q
+print('Hasil',p,'+',q,'=',hasil)
+print()
+
+a=23
+b=35
+hasil = a+b
+print('Hasil',a,'+',b,'=',hasil)
+print()
+
+a=21
+b=38
+hasil = a-b
+print('Hasil',a,'-',b,'=',hasil)
+print()
+
+a=12
+b=16
+hasil = a*b
+print('Hasil',a,'*',b,'=',hasil)
+print()
+
+a=5
+b=10
+hasil = a/b
+print('Hasil',a,'/',b,'=',hasil)
+print()
+
+a=34
+b=12
+hasil = a%b
+print('Hasil',a,'%',b,'=',hasil)
+print()
+
+a=2
+b=5
+hasil = a**b
+print('Hasil',a,'**',b,'=',hasil)
+print()
+
+a=11
+b=10
+hasil = a//b
+print('Hasil',a,'//',b,'=',hasil)
+print('Ini adalah batas untuk tugas dari operator aritmatika#####')
