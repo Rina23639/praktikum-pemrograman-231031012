2c4b50a810b10c07b3261581044bd9d789d2cd63
Rina23639 <153788278+Rina23639@users.noreply.github.com>
2023-12-21 09:05:42 +0700

    latihan-a3.py

---
+++ 231031012-rina-latihan3.py
@@ -0,0 +1,9 @@
+print('231031021-aulia-latihan3\n')
+
+nama = input('Masukkan nama karyawan: ')
+pendapatan = int(input('Masukkan pendapatan karyawan: '))
+
+if pendapatan > 1000:
+    print('Status: Berhak')
+else:
+    print('Status: Tidak Berhak')
