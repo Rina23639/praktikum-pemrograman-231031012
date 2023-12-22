79c43074d21b5803f8c1c989c019ce81254def87
Rina23639 <153788278+Rina23639@users.noreply.github.com>
2023-12-22 11:35:02 +0700

    tugas-SIA.py

---
+++ 231031012-tugas-SIA.py
@@ -0,0 +1,22 @@
+print('231031021-tugas\n')
+
+#TUGAS DICTIONARIES
+
+
+biodata = {
+    'nama':'Andi Aulia Shafira',
+    'nim':'231031021',
+    'prodi':'Sistem Informasi',
+    'tempat lahir':'Pinrang-30 Maret 2005',
+    'alamat':'JL. Lahalede NO. 90',
+    'pekerjaan':'Pelajar/Mahasiswa',
+    'hobi':'Membaca Buku Cerita/Fiksi',
+}
+
+print('Nama:',biodata['nama'])
+print('Nim:',biodata['nim'])
+print('Prodi:',biodata['prodi'])
+print('Tempat Lahir:',biodata['tempat lahir'])
+print('Alamat:',biodata['alamat'])
+print('Pekerjaan:',biodata['pekerjaan'])
+print('Hobi:',biodata['hobi'])
